04065fc14e9369aa2bf79cad40ed6a431b68a466
Brandon Martin-Anderson <badhill@gmail.com>
2018-05-11 19:22:48 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='censustable',
+      version='0.1',
+      description='Bulk import from the US Census FTP server.',
+      url='http://github.com/bmander/censustable',
+      author='Brandon Martin-Anderson',
+      author_email='bmander@gmail.com',
+      license='MIT',
+      packages=['censustable'],
+      zip_safe=False)
